8156117e28722976e805766ab55a8ce7fe819ffa
Trent Willis <twillis@linkedin.com>
2016-11-30 18:32:26 -0800

    Port code from ember-engines

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+import fs from 'fs';
+import path from 'path-posix';
 import Plugin from 'broccoli-plugin';
 import FSTree from 'fs-tree-diff';
+import Entry from 'fs-tree-diff/lib/entry';
+import existsSync from 'exists-sync';
 import heimdall from 'heimdalljs';
 import { default as _logger } from 'heimdalljs-logger';
+import rimraf from 'rimraf';
+import { rollup } from 'rollup';
+import { moduleResolve as amdNameResolver } from 'amd-name-resolver';
+
+import copyFile from './utils/copy-file';
+import existsStat from './utils/exists-stat';
+import filterDirectory from './utils/filter-directory';
 
 const logger = _logger('broccoli-dependency-funnel');
 
@@ -13,10 +24,144 @@ export default class BroccoliDependencyFunnel extends Plugin {
       persistentOutput: true
     });
 
-    // Save references to options you may need later
+    if (!(options.include ^ options.exclude)) {
+      throw new Error('Must specify exactly one of `include` or `exclude`.');
+    }
+
+    // An array and FSTree, respectively, representing the dependency graph of the
+    // entry point or the non-dependency graph.
+    this._depGraph = undefined;
+    this._depGraphTree = undefined;
+    this._nonDepGraph = undefined;
+    this._nonDepGraphTree = undefined;
+
+    this.options = options;
   }
 
   build() {
-    // The build logic of your plugin goes here
+      var inputPath = this.inputPaths[0];
+
+    // Check for changes in the files included in the rollup
+    if (this._depGraph) {
+      var incomingDepGraphTree = this._getFSTree(this._depGraph);
+      var depGraphPatch = this._depGraphTree.calculatePatch(incomingDepGraphTree);
+      var hasDepGraphChanges = depGraphPatch.length !== 0;
+
+      if (!hasDepGraphChanges) {
+        var incomingNonDepGraphTree = this._getFSTree(this._nonDepGraph);
+        var nonDepGraphPatch = this._nonDepGraphTree.calculatePatch(incomingNonDepGraphTree);
+        var hasNonDepGraphChanges = nonDepGraphPatch.length !== 0;
+
+        if (!hasNonDepGraphChanges) {
+          return;
+        }
+
+        if (this.options.include) {
+          return;
+        }
+
+        if (this.options.exclude) {
+          FSTree.applyPatch(inputPath, this.outputPath, nonDepGraphPatch);
+          return;
+        }
+      }
+    }
+
+    var modules = [];
+
+    var entryExists = existsSync(path.join(inputPath, this.options.rollup.entry));
+    if (!entryExists && this.options.include) {
+      return;
+    } else if (!entryExists && this.options.exclude) {
+      modules = fs.readdirSync(inputPath);
+      this.copy(modules);
+      return;
+    }
+
+    var rollupOptions = {
+      entry: this.options.rollup.entry,
+      external: this.options.rollup.external || [],
+      dest: 'foo.js',
+      plugins: [
+        {
+          resolveId: function(importee, importer) {
+            var moduleName;
+
+            // This will only ever be the entry point.
+            if (!importer) {
+              moduleName = importee.replace(inputPath, '');
+              modules.push(moduleName);
+              return path.join(inputPath, importee);
+            }
+
+            // Link in the global paths.
+            moduleName = amdNameResolver(importee, importer).replace(inputPath, '').replace(/^\//, '');
+            var modulePath = path.join(inputPath, moduleName + '.js');
+            if (existsSync(modulePath)) {
+              modules.push(moduleName + '.js');
+              return modulePath;
+            }
+          }
+        }
+      ]
+    };
+
+    return rollup(rollupOptions).then(function() {
+      var toCopy;
+
+      this._depGraph = modules.sort();
+      this._nonDepGraph = filterDirectory(inputPath, '', function(module) {
+        return modules.indexOf(module) === -1;
+      }).sort();
+
+      rimraf.sync(this.outputPath);
+
+      if (this.options.include) {
+        toCopy = this._depGraph;
+      }
+
+      if (this.options.exclude) {
+        toCopy = this._nonDepGraph;
+      }
+
+      this.copy(toCopy);
+
+      this._depGraphTree = this._getFSTree(this._depGraph);
+      this._nonDepGraphTree = this._getFSTree(this._nonDepGraph);
+
+      return;
+    }.bind(this));
+  }
+
+  copy(inodes) {
+    const inputPath = this.inputPaths[0];
+    const outputPath = this.outputPath;
+
+    for (let i = 0; i < inodes.length; i++) {
+      const module = inodes[i];
+      copyFile(path.join(inputPath, module), path.join(outputPath, module));
+    }
+  }
+
+  /**
+   * Constructs an FSTree from the passed in paths.
+   *
+   * @param {Array<String>} paths
+   * @return {FSTree}
+   */
+  _getFSTree(paths) {
+    var inputPath = this.inputPaths[0];
+    var entries = paths.map(function(entryPath) {
+      var absolutePath = path.join(inputPath, entryPath);
+      var stat = existsStat(absolutePath);
+
+      if (!stat) {
+        return;
+      }
+
+      return Entry.fromStat(entryPath, stat);
+    }).filter(Boolean);
+
+    return FSTree.fromEntries(entries);
   }
 }
--- tests/index.js
+++ tests/index.js
@@ -8,6 +8,7 @@ import chaiFiles from 'chai-files';
 import fixture from 'fixturify';
 import fs from 'fs-extra';
 import walkSync from 'walk-sync';
+import path from 'path';
 
 const { expect } = chai;
 const { file } = chaiFiles;
@@ -15,49 +16,260 @@ const { file } = chaiFiles;
 chai.config.truncateThreshold = 1000;
 chai.use(chaiFiles);
 
+// Some filesystems dont have lower then 1s mtime resolution
+async function fsTick() {
+  return new Promise((resolve) => setTimeout(resolve, 1001));
+}
+
+// Helper to assert that two stats are equal, but only in the ways we care about.
+// Helpful because atime is finicky.
+function assertStatEqual(a, b) {
+  expect(a.mtime.toString()).to.equal(b.mtime.toString());
+  expect(a.mode).to.equal(b.mode);
+  expect(a.size).to.equal(b.size);
+}
+
+// Helper to assert that a file did change. We really only care that mtime changed.
+function assertStatChange(a, b) {
+  expect(a.mtime.toString()).to.not.equal(b.mtime.toString());
+}
+
 describe('BroccoliDependencyFunnel', function() {
+  this.timeout(5000);
+
   const input = 'tmp/fixture-input';
   let node, pipeline;
 
   beforeEach(function() {
     fs.mkdirpSync(input);
     fixture.writeSync(input, {
-      // Your fixture directory structure
+      'routes.js': 'import buildRoutes from "ember-engines/routes";import foo from "utils/foo";',
+      'utils': {
+        'foo.js': 'import derp from "./derp";export default {};',
+        'derp.js': 'export default {};',
+        'herp.js': 'export default {};'
+      },
+      'engine.js': 'import herp from "./utils/herp";'
     });
-
-    node = new BroccoliDependencyFunnel(input, {
-      // Options
-    });
-
-    pipeline = new broccoli.Builder(node);
   });
 
   afterEach(function() {
     fs.removeSync(input);
-    return pipeline.cleanup();
+    return pipeline.cleanup().then(function() {
+      node = undefined;
+      pipeline = undefined;
+    });;
   });
 
   describe("build", function() {
-    it('simple', async function() {
+    it('returns a tree of the dependency graph when using include', async function() {
+      node = new BroccoliDependencyFunnel(input, {
+        include: true,
+        rollup: {
+          entry: 'routes.js',
+          external: [ 'ember-engines/routes' ]
+        }
+      });
+
+      pipeline = new broccoli.Builder(node);
+
       const { directory } = await pipeline.build();
 
-      // Use expect + walkSync/file to verify the output of your build
+      const output = walkSync(directory);
+      expect(output).to.deep.equal([ 'routes.js', 'utils/', 'utils/derp.js', 'utils/foo.js' ]);
+    });
+
+    it('returns a tree excluding the dependency graph when using exclude', async function() {
+      node = new BroccoliDependencyFunnel(input, {
+        exclude: true,
+        rollup: {
+          entry: 'routes.js',
+          external: [ 'ember-engines/routes' ]
+        }
+      });
+
+      pipeline = new broccoli.Builder(node);
+
+      const { directory } = await pipeline.build();
+
+      const output = walkSync(directory);
+      expect(output).to.deep.equal([ 'engine.js', 'utils/', 'utils/herp.js' ]);
     });
   });
 
   describe('rebuild', function() {
-    it('is stable on idempotent rebuild', async function() {
-      let { directory } = await pipeline.build();
+    it('is stable on unchanged rebuild with include', async function() {
+      node = new BroccoliDependencyFunnel(input, {
+        include: true,
+        rollup: {
+          entry: 'routes.js',
+          external: [ 'ember-engines/routes' ]
+        }
+      });
 
-      let beforeStat = fs.statSync(directory);
+      pipeline = new broccoli.Builder(node);
 
-      // Some filesystems dont have lower then 1s mtime resolution
-      await new Promise(resolve => setTimeout(resolve, 1000));
+      const { directory } = await pipeline.build();
+
+      const beforeStat = fs.statSync(directory);
+
+      await fsTick();
       await pipeline.build();
 
-      let afterStat = fs.statSync(directory);
+      const afterStat = fs.statSync(directory);
+      assertStatEqual(beforeStat, afterStat);
+    });
 
-      expect(beforeStat).to.deep.equal(afterStat);
+    it('is stable on unchanged rebuild with exclude', async function() {
+      node = new BroccoliDependencyFunnel(input, {
+        exclude: true,
+        rollup: {
+          entry: 'routes.js',
+          external: [ 'ember-engines/routes' ]
+        }
+      });
+
+      pipeline = new broccoli.Builder(node);
+
+      const { directory } = await pipeline.build();
+
+      const beforeStat = fs.statSync(directory);
+
+      await fsTick();
+      await pipeline.build();
+
+      const afterStat = fs.statSync(directory);
+      assertStatEqual(beforeStat, afterStat);
+    });
+
+    it('is stable when changes occur outside dep graph and using include', async function() {
+      node = new BroccoliDependencyFunnel(input, {
+        include: true,
+        rollup: {
+          entry: 'routes.js',
+          external: [ 'ember-engines/routes' ]
+        }
+      });
+
+      pipeline = new broccoli.Builder(node);
+      const { directory } = await pipeline.build();
+
+      const beforeStat = fs.statSync(directory);
+      await fsTick();
+
+      fixture.writeSync(input, {
+        'engine.js': ''
+      });
+
+      await pipeline.build();
+
+      const afterStat = fs.statSync(directory);
+      assertStatEqual(beforeStat, afterStat, 'stable rebuild when modifying file NOT in dep graph');
+    });
+
+    it('updates when changes occur in dep graph and using include', async function() {
+      node = new BroccoliDependencyFunnel(input, {
+        include: true,
+        rollup: {
+          entry: 'routes.js',
+          external: [ 'ember-engines/routes' ]
+        }
+      });
+
+      pipeline = new broccoli.Builder(node);
+      const { directory } = await pipeline.build();
+
+      const beforeStat = fs.statSync(directory);
+
+      let output = walkSync(directory);
+      expect(output).to.deep.equal([ 'routes.js', 'utils/', 'utils/derp.js', 'utils/foo.js' ]);
+
+      await fsTick();
+
+      fixture.writeSync(input, {
+        'routes.js': 'import herp from "utils/herp";'
+      });
+
+      await pipeline.build();
+
+      const afterStat = fs.statSync(directory);
+      assertStatChange(beforeStat, afterStat, 'instable rebuild when modifying file in dep graph');
+
+      output = walkSync(directory);
+      expect(output).to.deep.equal([ 'routes.js', 'utils/', 'utils/herp.js' ]);
+    });
+
+    it('updates when changes occur outside dep graph and using exclude', async function() {
+      node = new BroccoliDependencyFunnel(input, {
+        exclude: true,
+        rollup: {
+          entry: 'routes.js',
+          external: [ 'ember-engines/routes' ]
+        }
+      });
+
+      pipeline = new broccoli.Builder(node);
+      const { directory } = await pipeline.build();
+
+      const engineStat = fs.statSync(directory + '/engine.js');
+      const routesStat = fs.statSync(directory + '/utils/herp.js');
+
+      await fsTick();
+
+      fixture.writeSync(input, {
+        'engine.js': ''
+      });
+
+      await pipeline.build();
+
+      const engineRebuildStat = fs.statSync(directory + '/engine.js');
+      assertStatChange(engineStat, engineRebuildStat, 'engine.js changed');
+
+      let routesRebuildStat = fs.statSync(directory + '/utils/herp.js');
+      assertStatEqual(routesStat, routesRebuildStat, 'routes.js unchanged');
+
+      await fsTick();
+
+      fs.unlink(path.join(input, 'engine.js'));
+
+      await pipeline.build();
+
+      expect(fs.existsSync(directory + '/engine.js')).to.be.not.ok;
+
+      routesRebuildStat = fs.statSync(directory + '/utils/herp.js');
+      assertStatEqual(routesStat, routesRebuildStat, 'routes.js unchanged second time');
+    });
+
+    it('updates when changes occur in dep graph and using exclude', async function() {
+      node = new BroccoliDependencyFunnel(input, {
+        exclude: true,
+        rollup: {
+          entry: 'routes.js',
+          external: [ 'ember-engines/routes' ]
+        }
+      });
+
+      pipeline = new broccoli.Builder(node);
+      const { directory } = await pipeline.build();
+
+      const buildStat = fs.statSync(directory);
+
+      let output = walkSync(directory);
+      expect(output).to.deep.equal([ 'engine.js', 'utils/', 'utils/herp.js' ]);
+
+      await fsTick();
+
+      fixture.writeSync(input, {
+        'routes.js': 'import herp from "utils/herp";'
+      });
+
+      await pipeline.build();
+
+      const rebuildStat = fs.statSync(directory);
+      assertStatChange(rebuildStat, buildStat, 'instable rebuild when modifying file in dep graph');
+
+      output = walkSync(directory);
+      expect(output).to.deep.equal([ 'engine.js', 'utils/', 'utils/derp.js', 'utils/foo.js' ]);
     });
   });
 });
